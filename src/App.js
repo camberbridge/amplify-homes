e462badbc31b546f230076bf0936d9f36113f5d5
Abe <fuminora@909c4ad2f169.ant.amazon.com>
2022-01-22 17:59:13 +0900

    updated App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import { FAQItemCollection, NavBar, FAQTitle } from './ui-components'
 import React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react'; // 認証に必要
 
+import Amplify, { Storage } from 'aws-amplify';
+
 function App() {
+
+Amplify.configure({
+    Storage: {
+        AWSS3: {
+            bucket: 'amplify-pdf-polly-storage-dzek1dcdkq2li161951-devn', //REQUIRED -  Amazon S3 bucket name
+            region: 'us-east-1', //OPTIONAL -  Amazon service region
+        }
+    }
+});
+
+async function onChange(e) {
+  const file = e.target.files[0];
+  try {
+    await Storage.put(file.name, file, {
+      contentType: "application/pdf", // contentType is optional
+    });
+  } catch (error) {
+    console.log("Error uploading file: ", error);
+  }
+}
+
   return (
     <div className="App">
       <NavBar width={"100vw"}/>
       <FAQTitle width={"100vw"}/>
+      <input type="file" onChange={onChange} />
       <FAQItemCollection />
     </div>
   );
