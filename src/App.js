770ad8f0ad3f7314ff02559caa1b7a5fdd176544
Abe <fuminora@909c4ad2f169.ant.amazon.com>
2022-01-23 23:14:08 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ Amplify.configure({
     }
 });
 
-function htmlTemplate(fileName, mp3URL){
-  const text = '<div class="amplify-flex amplify-collection"><div class="amplify-flex amplify-collection-items" style="flex-direction: column; justify-content: stretch;"><div style="align-items: center; align-self: stretch; background-color: rgb(255, 255, 255); flex-direction: row; gap: 0px; height: 70px; padding: 24px; position: relative; flex-shrink: 0;" class="amplify-flex"><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 964px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 964px;" class="amplify-text">　■ '+fileName+'</p></div><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 97px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 97px;" class="amplify-text">'+'<a href="'+mp3URL+'">'+'Download MP3</a>'+'</p></div><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 97px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 97px;" class="amplify-text">Delete</p></div></div><hr style="align-self: stretch; flex-shrink: 0; width: 1280px;" aria-orientation="horizontal" class="amplify-divider" data-size="small"></div></div>';
+function htmlTemplate(fileName, mp3URL, uid){
+  const text = '<div class="amplify-flex amplify-collection"><div class="amplify-flex amplify-collection-items" style="flex-direction: column; justify-content: stretch;"><div style="align-items: center; align-self: stretch; background-color: rgb(255, 255, 255); flex-direction: row; gap: 0px; height: 70px; padding: 24px; position: relative; flex-shrink: 0;" class="amplify-flex"><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 964px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 964px;" class="amplify-text">　■ '+fileName+'</p></div><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 97px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 97px;" class="amplify-text">'+'<a href="'+mp3URL+'">'+'Download MP3</a>'+'</p></div><div style="flex-direction: column; gap: 16px; height: 46px; padding: 0px; position: relative; flex-shrink: 0; width: 97px;" class="amplify-flex"><p style="align-self: stretch; color: rgb(0, 0, 0); flex-direction: column; display: flex; font-family: Inter; font-size: 16px; font-weight: 700; justify-content: flex-start; line-height: 24px; padding: 0px; position: relative; flex-shrink: 0; text-align: left; width: 97px;" class="amplify-text"><button id="'+uid+'" onClick="deleteDDBItem(this);">Delete</button></p></div></div><hr style="align-self: stretch; flex-shrink: 0; width: 1280px;" aria-orientation="horizontal" class="amplify-divider" data-size="small"></div></div>';
   return(text);
 }
 
 var htmlTexts = '';
+var AWS = require("aws-sdk");
 function init(){
-  var AWS = require("aws-sdk");
   AWS.config.update({
     region: "us-east-1",
     accessKeyId: amplify.testId,
@@ -56,7 +56,8 @@ function init(){
               console.log('File name : ' + dt.address);
               console.log('MP3 file URL : ' + dt.image_url);
               console.log('createdAt : ' + dt.createdAt);
-              doc_list.push({'doc':{'file_name':dt.address, 'mp3_url':dt.image_url, 'create_at':dt.createdAt}});
+              console.log('uid : ' + dt.id);
+              doc_list.push({'doc':{'file_name':dt.address, 'mp3_url':dt.image_url, 'create_at':dt.createdAt, 'uid':dt.id}});
         });
         var doc_list_r = doc_list.sort((d, next_d)=>{
           return d.doc.create_at > next_d.doc.create_at ? -1 : 1;
@@ -65,7 +66,7 @@ function init(){
           const mp3_url = dt.doc.mp3_url
           console.log('=========');
           console.log(mp3_url);
-          htmlTexts += htmlTemplate(dt.doc.file_name, mp3_url);
+          htmlTexts += htmlTemplate(dt.doc.file_name, mp3_url, dt.doc.uid);
         });
       }
 
@@ -73,6 +74,40 @@ function init(){
   });
 }
 init();
+
+
+
+// function deleteDDBItem(element){
+//   //https://www.wakuwakubank.com/posts/670-nodejs-dynamodb/
+//   const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+//   const TABLE_NAME = amplify.ddbTable;
+
+//   // Following, query an item.
+//   var params = {
+//     TableName : TABLE_NAME,
+//     FilterExpression: 'image_url = :image_url',
+//     ExpressionAttributeValues: { ':image_url': 'https://1.bp.blogspot.com/-CKc-Trn35FE/X9lJKrd_jeI/AAAAAAABc1w/GbDHjFtUzDY6OZRl1FnS3FQVwJz2Scl7gCNcBGAsYHQ/s180-c/animal_black_sheep_hitsuji.png' }
+//   };
+//   docClient.scan(params, (err, data) => {
+//     console.log('DDBクエリライン');
+//     if (err) console.log(JSON.stringify(err, null, 2))
+//     else console.log(JSON.stringify(data, null, 2))
+//   });
+
+//   // Following, delete an item.
+//   params = {
+//     TableName : TABLE_NAME, // テーブル名
+//     Key:{
+//       id: element.id
+//     }
+//   };
+//   docClient.delete(params, (err, data) => {
+//     console.log('DDB削除ライン');
+//     if (err) console.log(JSON.stringify(err, null, 2))
+//     else console.log(JSON.stringify(data, null, 2))
+//   });
+// }
+
 
 function App() {
   async function onChange(e) {
